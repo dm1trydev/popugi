bf97fdab8165315f171fd2ee2982cea73790f1e0
Dmitry Kuznetsov <Dmitriy.Kuznetsov@bia-tech.ru>
2021-11-13 16:19:19 +0300

    Transfer schemas to monorepo root

--- tracker/config/initializers/schema_registry.rb
+++ tracker/config/initializers/schema_registry.rb
@@ -1,5 +1,5 @@
 SchemaRegistry.module_eval do
   def self.loader
-    @loader ||= SchemaRegistry::Loader.new(schemas_root_path: Rails.root.join('schemas'))
+    @loader ||= SchemaRegistry::Loader.new(schemas_root_path: Rails.root.join('..', 'schemas'))
   end
 end
